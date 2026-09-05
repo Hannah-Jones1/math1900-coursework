f71351fd0d0cd0f17246d587a1f9bd1cf62964d4
Nannybug <hjones3@clevelandstatecc.edu>
2026-09-04 21:48:24 -0400

    Add Homework 1 solution

---
+++ Homework_Set_1/Math 1900 HW1 Jones.cpp
@@ -0,0 +1,17 @@
+/* Hannah Jones
+Math 1900 Homework 1
+Module 1: getting started with C++ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cout << "Student: Hannah Jones" << endl;
+	cout << "Course: Math 1900 - Scientific Programming" << endl;
+	cout << "Experiment: Newtons 3rd Law Verification " << endl;
+	cout << "Date: 9/4/2026";
+
+// returning the value to zero and ending the program
+	return 0;
+}
